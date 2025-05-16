94edd39d155f46a32122db46722dcd1b01a3f9d9
Enric Cervera <ecervera@uji.es>
2025-05-16 15:31:42 +0200

    Prepare templates

---
+++ Notebooks/ZS6D/prepare_templates_and_gt.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
+export DISPLAY=:1
+python3 /code/ZS6D/prepare_templates_and_gt.py --config_file /code/ZS6D/zs6d_configs/template_gt_preparation_configs/cfg_template_gt_generation_ycbv.json 
+
