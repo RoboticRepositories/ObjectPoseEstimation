cea4e52015c5ca1638bbd16ff7c800033c0804f8
Enric Cervera <ecervera@uji.es>
2025-05-16 12:19:34 +0200

    Scripts for creating and running Docker image

---
+++ Docker/ZS6D/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+docker build -t op_zs6d .
+
---
+++ Docker/ZS6D/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+docker run -it --rm --network host --gpus all \
+  -v ./Notebooks/ZS6D:/Notebooks -v $HOME/.cache/torch:/root/.cache/torch \
+  op_zs6d jupyter notebook --no-browser --allow-root
+
+
