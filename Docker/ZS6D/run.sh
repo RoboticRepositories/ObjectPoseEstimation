9d9e7ada8ce133d3373f20ef435b63fcb844831f
Enric Cervera <ecervera@uji.es>
2025-05-16 15:41:15 +0200

    Open Jupyter in local browser

--- Docker/ZS6D/run.sh
+++ Docker/ZS6D/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
-docker run -it --rm --network host --gpus all \
+docker run --rm --network host --gpus all \
   -v ./Notebooks/ZS6D:/Notebooks \
   -v ./ZS6D:/code/ZS6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
-  op_zs6d jupyter notebook --no-browser --allow-root
+  op_zs6d jupyter notebook --no-browser --allow-root \
+  --NotebookApp.token='' &
 
+sleep 1 && xdg-open http://localhost:8888/tree
 
