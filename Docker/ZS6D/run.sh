989dc85ca4153be08b4d635d4a1d41adef6a55cf
Enric Cervera <ecervera@uji.es>
2025-05-16 16:06:47 +0200

    Update token option

--- Docker/ZS6D/run.sh
+++ Docker/ZS6D/run.sh
@@ -5,5 +5,5 @@ docker run --rm --network host --gpus all \
   -v ./ZS6D:/code/ZS6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
   op_zs6d jupyter notebook --no-browser --allow-root \
-  --NotebookApp.token=''
+  --IdentityProvider.token=''
 
