f2ece09f112838420994de4ea9c6113bfc2db616
Enric Cervera <ecervera@uji.es>
2025-05-16 16:03:20 +0200

    Do not open browser

--- Docker/ZS6D/run.sh
+++ Docker/ZS6D/run.sh
@@ -5,7 +5,5 @@ docker run --rm --network host --gpus all \
   -v ./ZS6D:/code/ZS6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
   op_zs6d jupyter notebook --no-browser --allow-root \
-  --NotebookApp.token='' &
-
-sleep 1 && xdg-open http://localhost:8888/tree
+  --NotebookApp.token=''
 
