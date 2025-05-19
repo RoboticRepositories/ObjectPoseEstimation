2d02d9a464e491ac0e069791bc3079edf86f527b
Enric Cervera <ecervera@uji.es>
2025-05-19 09:42:53 +0200

    Rename workspace

--- Docker/SAM-6D/run.sh
+++ Docker/SAM-6D/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 docker run --rm --network host --gpus all \
-  -v ./Notebooks/SAM-6D:/Notebooks \
+  -v ./Workspace/SAM-6D:/Workspace \
   -v ./SAM-6D/SAM-6D:/code/SAM-6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
   op_sam-6d sh -c ". /root/.bashrc && conda activate sam6d && jupyter notebook --no-browser --allow-root --IdentityProvider.token=''"
--- Docker/ZS6D/run.sh
+++ Docker/ZS6D/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 docker run --rm --network host --gpus all \
-  -v ./Notebooks/ZS6D:/Notebooks \
+  -v ./Workspace/ZS6D:/Workspace \
   -v ./ZS6D:/code/ZS6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
   op_zs6d jupyter notebook --no-browser --allow-root \
