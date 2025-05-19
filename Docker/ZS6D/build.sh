8499451c1cdd88ec35e436df642887c07248af07
Enric Cervera <ecervera@uji.es>
2025-05-19 10:00:55 +0200

    Update tag

--- Docker/ZS6D/build.sh
+++ Docker/ZS6D/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker build -t op_zs6d -f ./Docker/ZS6D/Dockerfile .
+docker build -t robinlab/op_zs6d -f ./Docker/ZS6D/Dockerfile .
 
--- Docker/ZS6D/run.sh
+++ Docker/ZS6D/run.sh
@@ -4,6 +4,6 @@ docker run --rm --network host --gpus all \
   -v ./Workspace/ZS6D:/Workspace \
   -v ./ZS6D:/code/ZS6D \
   -v $HOME/.cache/torch:/root/.cache/torch \
-  op_zs6d jupyter notebook --no-browser --allow-root \
+  robinlab/op_zs6d jupyter notebook --no-browser --allow-root \
   --IdentityProvider.token=''
 
