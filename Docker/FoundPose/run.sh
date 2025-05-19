bb7b7291bc65b4bcc294043d933585e8216b3618
Enric Cervera <ecervera@uji.es>
2025-05-19 17:09:36 +0200

    Use OSRF/rocker for template generation

--- Docker/FoundPose/run.sh
+++ Docker/FoundPose/run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
-docker run --rm --network host --gpus all \
-  -v ./Datasets:/Datasets \
-  -v ./Workspace/FoundPose:/Workspace \
-  -v ./foundpose:/code/foundpose \
-  -v $HOME/.cache/torch:/root/.cache/torch \
-  robinlab/op_foundpose jupyter notebook --no-browser --allow-root \
-  --IdentityProvider.token=''
+#docker run --rm --network host --gpus all \
+#  -v ./Datasets:/Datasets \
+#  -v ./Workspace/FoundPose:/Workspace \
+#  -v ./foundpose:/code/foundpose \
+#  -v $HOME/.cache/torch:/root/.cache/torch \
+#  robinlab/op_foundpose jupyter notebook --no-browser --allow-root \
+#  --IdentityProvider.token=''
+
+rocker --x11 --nvidia --network host \
+  --volume ./Datasets:/Datasets ./Workspace/FoundPose:/Workspace \
+           ./foundpose:/code/foundpose $HOME/.cache/torch:/root/.cache/torch -- \
+  robinlab/op_foundpose jupyter notebook --no-browser --allow-root --IdentityProvider.token=''
 
